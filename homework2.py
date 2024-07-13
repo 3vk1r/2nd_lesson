c21348cce95d4d56b54663c625f4a5bb4ad2d74c
3vk1r <135658009+3vk1r@users.noreply.github.com>
2024-07-13 10:12:30 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+complete_HW = 12
+hours = 1.5
+name_course = 'Python'
+time_for_one_tasks = hours / complete_HW
+
+print(f'{name_course}, всего задач: {complete_HW}, затрачено часов: {hours}, среднее время выполнения {time_for_one_tasks} часа')
